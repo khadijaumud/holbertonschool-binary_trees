ae915569c4f042ff3e9474c9b95c27a61c7321df
Natiq7855 <natabbasli@gmail.com>
2026-01-28 07:15:27 +0000

    14 th task first commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_h = 0, right_h = 0;
+int left_h = 0, right_h = 0;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left)
-        left_h = (int)binary_tree_height(tree->left) + 1;
+if (tree == NULL)
+return (0);
+if (tree->left)
+left_h = (int)binary_tree_height(tree->left) + 1;
     
-    if (tree->right)
-        right_h = (int)binary_tree_height(tree->right) + 1;
+if (tree->right)
+right_h = (int)binary_tree_height(tree->right) + 1;
 
-    return (left_h - right_h);
+return (left_h - right_h);
 }
