e1938afd75241c24d2fe05226c05306c8d008451
Natiq7855 <natabbasli@gmail.com>
2026-01-28 06:14:51 +0000

    11th task correction of betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,15 +10,15 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t right_size, left_size;
+size_t right_size, left_size;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+if (tree == NULL)
+{
+return (0);
+}
 
-    right_size = binary_tree_size(tree->right);
-    left_size = binary_tree_size(tree->left);
+right_size = binary_tree_size(tree->right);
+left_size = binary_tree_size(tree->left);
 
-    return (1 + left_size + right_size);
+return (1 + left_size + right_size);
 }
